c58f60ea71b0ee9fd41d8a61103618817f8b93cd
Costigun <maximneveraa@gmail.com>
2021-04-19 14:01:34 +0000

    commit from digital ocean server

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&hojpx1zt1!8wi3vpjjf-hn+eyyjv4c8n6h_oxx6#%6!jld(zk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'django_movie.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'my_db',
+        'USER': 'maksim',
+        'PASSWORD': '123456',
+        'HOST':'localhost',
+        'PORT': 5432
     }
 }
 
@@ -120,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL='/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
