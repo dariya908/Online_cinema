6f9700bac25c71551914c1f64fe5bfe2736a71d7
Costigun <maximneveraa@gmail.com>
2021-04-21 19:26:08 +0600

    commit from local machine

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -77,15 +77,10 @@ WSGI_APPLICATION = 'django_movie.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'my_db',
-        'USER': 'maksim',
-        'PASSWORD': '123456',
-        'HOST':'localhost',
-        'PORT': 5432
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
 }
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
